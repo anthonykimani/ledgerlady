a540b106ce8ba440fdda9dbb2be7d9c19244584f
anthonykimani <kimaniantoni@gmail.com>
2024-07-25 11:45:34 +0300

    setups next-app with shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,65 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        DM: ["DM Sans", "sans-serif"],
+        clash: ["Clash Display", "sans-serif"],
+        clashDisplay: ['var(--font-clashDisplay)'],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      screens: {
+        xxs: "320px",
+        // => @media (min-width: 320px) { ... }
+  
+        xsm: "450px",
+        // => @media (min-width: 450px) { ... }
+  
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+  
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+  
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+  
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+  
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
